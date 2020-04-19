1de45be129da5013a5c84a863a5104727e94c4f5
Ben Budomo <luminous.talisman@protonmail.com>
2020-04-18 23:39:32 -0500

    Create basic operator functions

---
+++ script.js
@@ -0,0 +1,31 @@
+function addNumbers(numberOne, numberTwo) {
+    console.log(Number(numberOne) + Number(numberTwo));
+}
+
+function subtractNumbers(numberOne, numberTwo) {
+    console.log(Number(numberOne) - Number(numberTwo));
+}
+
+function multiplyNumbers(numberOne, numberTwo) {
+    console.log(Number(numberOne) * Number(numberTwo));
+}
+
+function divideNumbers(numberOne, numberTwo) {
+    console.log(Number(numberOne) / Number(numberTwo));
+}
+
+function operate () {
+    let userOperator = prompt('Operator?');
+    let userFirstNumber = prompt('First Number?');
+    let userSecondNumber = prompt('Second Number?');
+
+    if(userOperator == 'add') {
+        addNumbers(userFirstNumber, userSecondNumber);
+    } else if(userOperator == 'subtract') {
+        subtractNumbers(userFirstNumber, userSecondNumber);
+    } else if(userOperator == 'multiply') {
+        multiplyNumbers(userFirstNumber, userSecondNumber);
+    } else if(userOperator == 'divide') {
+        divideNumbers(userFirstNumber, userSecondNumber);
+    }
+}
